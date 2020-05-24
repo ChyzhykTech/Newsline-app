8a78fa6fe4d1dd7e6e341cfc35bdb78ec270b33f
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-24 22:58:08 +0300

    Update CommentsController.cs

--- backend/Thread .NET.WebAPI/Controllers/CommentsController.cs
+++ backend/Thread .NET.WebAPI/Controllers/CommentsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Thread_.NET.BLL.Services;
 using Thread_.NET.Common.DTO.Comment;
+using Thread_.NET.Common.DTO.Like;
 using Thread_.NET.Extensions;
 
 namespace Thread_.NET.WebAPI.Controllers
@@ -13,10 +14,13 @@ namespace Thread_.NET.WebAPI.Controllers
     public class CommentsController : ControllerBase
     {
         private readonly CommentService _commentService;
+        private readonly LikeService _likeService;
 
-        public CommentsController(CommentService commentService)
+        public CommentsController(CommentService commentService,
+            LikeService likeService)
         {
             _commentService = commentService;
+            _likeService = likeService;
         }
 
         [HttpPost]
@@ -32,6 +36,15 @@ namespace Thread_.NET.WebAPI.Controllers
             var succeed = await _commentService.DeleteComment(commentId);
             if (!succeed) return NotFound();
             return NoContent();
+        }
+
+        [HttpPost("like")]
+        public async Task<IActionResult> LikeComment(NewReactionDTO reaction)
+        {
+            reaction.UserId = this.GetUserIdFromToken();
+
+            await _likeService.LikeComment(reaction);
+            return Ok();
         }
     }
 }
