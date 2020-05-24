0265fac44f56b4ff93caf7bdd7219fd0930d8531
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-24 22:58:47 +0300

    Update like.service.ts

--- frontend/src/app/services/like.service.ts
+++ frontend/src/app/services/like.service.ts
@@ -4,13 +4,45 @@ import { Post } from '../models/post/post';
 import { NewReaction } from '../models/reactions/newReaction';
 import { PostService } from './post.service';
 import { User } from '../models/user';
+import { Comment } from '../models/comment/comment';
 import { map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { CommentService } from './comment.service';
 
 @Injectable({ providedIn: 'root' })
 export class LikeService {
-    public constructor(private authService: AuthenticationService, private postService: PostService) {}
+    public constructor(private authService: AuthenticationService, private postService: PostService,
+        private commentService: CommentService) {}
 
+    public likeComment(comment: Comment, currentUser: User) {
+        const innerComment= comment;
+
+        const reaction: NewReaction = {
+            entityId: innerComment.id,
+            isLike: true,
+            userId: currentUser.id
+        };
+
+        // update current array instantly
+        let hasReaction = innerComment.reactions.some((x) => x.user.id === currentUser.id);
+        innerComment.reactions = hasReaction
+            ? innerComment.reactions.filter((x) => x.user.id !== currentUser.id)
+            : innerComment.reactions.concat({ isLike: true, user: currentUser });
+        hasReaction = innerComment.reactions.some((x) => x.user.id === currentUser.id);
+
+        return this.commentService.likeComment(reaction).pipe(
+            map(() => innerComment),
+            catchError(() => {
+                // revert current array changes in case of any error
+                innerComment.reactions = hasReaction
+                    ? innerComment.reactions.filter((x) => x.user.id !== currentUser.id)
+                    : innerComment.reactions.concat({ isLike: true, user: currentUser });
+
+                return of(innerComment);
+            })
+        );
+    }
+    
     public likePost(post: Post, currentUser: User) {
         const innerPost = post;
 
