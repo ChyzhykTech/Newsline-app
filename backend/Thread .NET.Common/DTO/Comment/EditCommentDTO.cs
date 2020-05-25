14ada73e9a31f859f3b6db955ceb5e29ec6ec6fd
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-25 15:46:58 +0300

    Create EditCommentDTO.cs

---
+++ backend/Thread .NET.Common/DTO/Comment/EditCommentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Thread_.NET.Common.DTO.Comment
+{
+    public sealed class EditCommentDTO
+    {
+        public int PostId { get; set; }
+        public string Body { get; set; }
+    }
+}
