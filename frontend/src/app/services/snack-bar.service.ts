f71590d5ccca5b56c0ec3a5bf5851aeca3cdc61b
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-05 15:21:37 +0300

    Update snack-bar.service.ts

--- frontend/src/app/services/snack-bar.service.ts
+++ frontend/src/app/services/snack-bar.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { LikeSnackbarComponent } from '../components/like-snackbar/like-snackbar.component';
+import { Post } from '../models/post/post';
+import { User } from '../models/user';
 
 @Injectable({ providedIn: 'root' })
 export class SnackBarService {
@@ -11,5 +14,11 @@ export class SnackBarService {
 
     public showUsualMessage(message: any) {
         this.snackBar.open(message, '', { duration: 3000, panelClass: 'usual-snack-bar' });
+    }
+
+    public showLikeMessage(post: Post, userName: string, avatar: string) {
+        // if(post.author.id !== userId) return;
+        this.snackBar.openFromComponent(LikeSnackbarComponent,  
+            {duration: 10000 , horizontalPosition: 'right', data: {...post, userName, avatar} });
     }
 }
