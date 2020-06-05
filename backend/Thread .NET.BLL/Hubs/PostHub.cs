421a19f5c7a368453e58833a3743b6eabbb9a990
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-05 15:20:23 +0300

    Update PostHub.cs

--- backend/Thread .NET.BLL/Hubs/PostHub.cs
+++ backend/Thread .NET.BLL/Hubs/PostHub.cs
@@ -1,19 +1,40 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System.Linq;
 using System.Threading.Tasks;
+using Thread_.NET.BLL.Services;
 using Thread_.NET.Common.DTO.Post;
 
 namespace Thread_.NET.BLL.Hubs
 {
+    [Authorize]
     public sealed class PostHub : Hub
     {
+        private readonly UserService _userService;
+
+        public PostHub(UserService userService)
+        {
+            _userService = userService;
+        }
+
         public async Task Send(PostDTO post)
         {
             await Clients.All.SendAsync("NewPost", post);
         }
-
-        public async Task SendPostLike(int postId)
+      
+        public async Task SendPostLike()
         {
-            await Clients.All.SendAsync("PostLike", postId);
+            var fromUserId = Context.User.Claims.FirstOrDefault(x => x.Type == "id")?.Value;
+            var userDTO = await _userService.GetUserById(int.Parse(fromUserId));
+            await Clients.All.SendAsync("PostLike", userDTO);
+        }
+
+        //For debug
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат");
+            await base.OnConnectedAsync();
         }
     }
 }
