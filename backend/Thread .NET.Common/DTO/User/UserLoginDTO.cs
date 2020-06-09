aa59e188032945a3abd3d97ec2650a8cf0d40dfe
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-09 08:56:13 +0300

    Update UserLoginDTO.cs

--- backend/Thread .NET.Common/DTO/User/UserLoginDTO.cs
+++ backend/Thread .NET.Common/DTO/User/UserLoginDTO.cs
@@ -4,11 +4,8 @@ namespace Thread_.NET.Common.DTO.User
 {
     public sealed class UserLoginDTO
     {
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
         
-        [Required]
         public string Password { get; set; }
     }
 }
