fd1a70577ca3e0adb039b49781df4d98f780af10
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-09 08:56:16 +0300

    Update UserRegisterDTO.cs

--- backend/Thread .NET.Common/DTO/User/UserRegisterDTO.cs
+++ backend/Thread .NET.Common/DTO/User/UserRegisterDTO.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Thread_.NET.Common.DTO.User
+﻿namespace Thread_.NET.Common.DTO.User
 {
     public sealed class UserRegisterDTO : UserDTO
     {
-        [Required]
-        [MinLength(6)]
         public string Password { get; set; }
     }
 }
