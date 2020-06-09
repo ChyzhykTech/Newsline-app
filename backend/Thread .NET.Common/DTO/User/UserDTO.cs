5c132d7f14f61c44d8a43bb3c624912061750b36
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-09 08:56:11 +0300

    Update UserDTO.cs

--- backend/Thread .NET.Common/DTO/User/UserDTO.cs
+++ backend/Thread .NET.Common/DTO/User/UserDTO.cs
@@ -1,21 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Thread_.NET.Common.DTO.User
+﻿namespace Thread_.NET.Common.DTO.User
 {
     public class UserDTO
     {
         public int Id { get; set; }
-        
-        [Required]
+
         public string Avatar { get; set; }
-        
-        [Required]
-        [EmailAddress]
+
         public string Email { get; set; }
        
-        [Required]
-        [MinLength(6)]
-        [MaxLength(20)]
         public string UserName { get; set; }
     }
 }
