a997c6559ac6bf6189c70247862dc08a8bcfafd6
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-12 09:33:08 +0300

    Update AuthController.cs

--- backend/Thread .NET.WebAPI/Controllers/AuthController.cs
+++ backend/Thread .NET.WebAPI/Controllers/AuthController.cs
@@ -3,25 +3,46 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Thread_.NET.BLL.Services;
 using Thread_.NET.Common.DTO.User;
+using Thread_.NET.Extensions;
 
 namespace Thread_.NET.WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    [AllowAnonymous]
+    [Authorize]
     [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly AuthService _authService;
+        private readonly EmailService _emailService;
 
-        public AuthController(AuthService authService)
+        public AuthController(AuthService authService, EmailService emailService)
         {
             _authService = authService;
+            _emailService = emailService;
         }
-
+     
         [HttpPost("login")]
+        [AllowAnonymous]
         public async Task<ActionResult<AuthUserDTO>> Login(UserLoginDTO dto)
         {
             return Ok(await _authService.Authorize(dto));
+        }
+
+        //[HttpGet("password/reset")]
+        //[AllowAnonymous]
+        //public async Task<ActionResult> Reset(string token)
+        //{
+
+        //}
+
+        [HttpPost("password/forgot")]
+        public async Task<ActionResult> ForgotPassword()
+        {
+            var userId = this.GetUserIdFromToken();
+            var protocol = Request.IsHttps ? "https" : "http";
+            var uri = $"{protocol}://{Request.Host.Host + Request.Host.Port}/api/auth/password/reset";
+            await _emailService.SendResetPasswordLinkToEmail(userId, uri);
+            return Ok();
         }
     }
 }
