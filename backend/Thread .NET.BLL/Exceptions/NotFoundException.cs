1555f2227aab08e8e74c009d468f7de2f7cb1250
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-14 18:11:32 +0300

    Update NotFoundException.cs

--- backend/Thread .NET.BLL/Exceptions/NotFoundException.cs
+++ backend/Thread .NET.BLL/Exceptions/NotFoundException.cs
@@ -2,7 +2,7 @@
 
 namespace Thread_.NET.BLL.Exceptions
 {
-    public sealed class NotFoundException : Exception
+    public class NotFoundException : Exception
     {
         public NotFoundException(string name, int id)
             : base($"Entity {name} with id ({id}) was not found.")
