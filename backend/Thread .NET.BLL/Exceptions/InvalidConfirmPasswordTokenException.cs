c498b136eef8324aa2ee2a585eb7a85600d3df6a
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-14 18:11:28 +0300

    Create InvalidConfirmPasswordTokenException.cs

---
+++ backend/Thread .NET.BLL/Exceptions/InvalidConfirmPasswordTokenException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Thread_.NET.BLL.Exceptions
+{
+    public sealed class InvalidConfirmPasswordTokenException : Exception
+    {
+        public InvalidConfirmPasswordTokenException() : base("Invalid confirm password token exception.") { }
+    }
+}
