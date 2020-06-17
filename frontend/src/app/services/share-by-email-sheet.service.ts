1e37b2568bf7b113b6fd155cd09ef83b1be5559d
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-17 18:46:41 +0300

    Update share-by-email-sheet.service.ts

--- frontend/src/app/services/share-by-email-sheet.service.ts
+++ frontend/src/app/services/share-by-email-sheet.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
-import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import {MatBottomSheet} from '@angular/material/bottom-sheet';
 import { ShareByEmailSheetComponent } from '../components/share-by-email-sheet/share-by-email-sheet.component';
+import { Post } from '../models/post/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShareByEmailSheetService {
 
-  constructor(
-    private _bottomSheet: MatBottomSheet,
-    private _bottomSheetRef: MatBottomSheetRef<ShareByEmailSheetComponent>
-  ) { }
+  constructor(private _bottomSheet: MatBottomSheet) { }
 
-  public open() {
-    this._bottomSheet.open(ShareByEmailSheetComponent);
+  public open(post: Post) {
+    this._bottomSheet.open(ShareByEmailSheetComponent, {data: post});
   }
 
   public close() {
-    this._bottomSheetRef.dismiss();
+    this._bottomSheet.dismiss();
   }
 }
