dcbcf74e64a3c2750df992763587ddc62b6d1cf8
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-17 18:46:25 +0300

    Update share-by-email-sheet.component.ts

--- frontend/src/app/components/share-by-email-sheet/share-by-email-sheet.component.ts
+++ frontend/src/app/components/share-by-email-sheet/share-by-email-sheet.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { ShareByEmailSheetService } from 'src/app/services/share-by-email-sheet.service';
 import { PostService } from 'src/app/services/post.service';
 import { takeUntil } from 'rxjs/operators';
 import { SnackBarService } from 'src/app/services/snack-bar.service';
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { Post } from 'src/app/models/post/post';
+import { SharePostByEmail } from 'src/app/models/post/share-post-by-email';
 
 @Component({
   selector: 'app-share-by-email-sheet',
   templateUrl: './share-by-email-sheet.component.html',
-  styleUrls: ['./share-by-email-sheet.component.css']
+  styleUrls: ['./share-by-email-sheet.component.sass']
 })
 export class ShareByEmailSheetComponent implements OnInit, OnDestroy {
   public email: string = "";
@@ -19,17 +22,19 @@ export class ShareByEmailSheetComponent implements OnInit, OnDestroy {
   public submitted: boolean = false;
 
   public shareForm: FormGroup;
-
+  private post: Post;
   private unsubscribe$ = new Subject<void>();
 
   constructor(
+    @Inject(MAT_BOTTOM_SHEET_DATA) private data: Post,
     private sheetService: ShareByEmailSheetService,
     private snackbarService: SnackBarService,
     private formBuilder: FormBuilder,
     private postService: PostService
-  ) { }
+  ) {  }
 
   ngOnInit(): void {
+    this.post = this.data;
     this.shareForm = this.initFormGroup();
   }
 
@@ -45,14 +50,22 @@ export class ShareByEmailSheetComponent implements OnInit, OnDestroy {
     if (this.shareForm.invalid) return;
 
     this.loading = true;
-    this.postService.sharePostByEmail(this.email)
+
+    let sharePost = {
+      post: this.post,
+      email: this.email
+    } as SharePostByEmail;
+
+    console.log(sharePost);
+
+    this.postService.sharePostByEmail(sharePost)
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe((resp) => {
         if(resp) {
-          this.snackbarService.showSuccessMessage(`Sent to email: ${this.email}`);
+          this.snackbarService.showSuccessMessage(`Post was sent to email: ${this.email}`);
           this.email = undefined;
           this.loading = false;
-          this.close();
+          this.cancel();
         }
       }, (err) => {
         this.snackbarService.showErrorMessage(err);
@@ -60,13 +73,13 @@ export class ShareByEmailSheetComponent implements OnInit, OnDestroy {
       })
   }
 
-  public close() {
+  public cancel() {
     this.sheetService.close();
   }
 
   private initFormGroup() {
     return this.formBuilder.group({
-      email: ['', [Validators.email]]
+      email: ['', [Validators.email, Validators.required]]
     });
   }
 }
