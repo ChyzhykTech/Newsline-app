f442d8952d8b19607f35499f687a62643d7e5dab
SergeySerg123 <sergey_chizhik@ukr.net>
2020-06-21 19:56:30 +0300

    Create infinity-scroll.component.ts

---
+++ frontend/src/app/components/infinity-scroll/infinity-scroll.component.ts
@@ -0,0 +1,51 @@
+import { Component, Inject, PLATFORM_ID, ElementRef, ViewChild, Output, EventEmitter, Input, OnDestroy, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { InfiniteScrollOptions } from 'src/app/helpers/InfiniteScrollOptions';
+
+@Component({
+  selector: 'app-infinity-scroll',
+  templateUrl: './infinity-scroll.component.html',
+  styleUrls: ['./infinity-scroll.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InfinityScrollComponent implements AfterViewInit, OnDestroy {
+
+  @Input() options: Partial<InfiniteScrollOptions> = {};
+
+  @Output() scrolled = new EventEmitter<void>();
+
+  @ViewChild('anchor') anchor: ElementRef<HTMLElement>;
+
+  private observer!: IntersectionObserver;
+
+  constructor(
+    private host: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.observer.disconnect();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.observer = new IntersectionObserver(([entry]) => entry.isIntersecting && this.scrolled.emit(), {
+        root: this.isHostScrollable() ? this.host.nativeElement : null,
+        ...this.options
+      });
+      this.observer.observe(this.anchor.nativeElement);
+    }
+  }
+
+  private isHostScrollable(): boolean {
+    if (isPlatformBrowser(this.platformId)) {
+      const style = window.getComputedStyle(this.host.nativeElement);
+
+      return style.getPropertyValue('overflow') === 'auto' || style.getPropertyValue('overflow-y') === 'scroll';
+    }
+
+    return false;
+  }
+}
