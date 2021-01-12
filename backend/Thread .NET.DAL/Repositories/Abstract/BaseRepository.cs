ef1f32510b927910e343e2a85e924cd0367d4282
Sergey <sergey_chizhik@ukr.net>
2021-01-12 15:02:18 +0200

    Create BaseRepository.cs

---
+++ backend/Thread .NET.DAL/Repositories/Abstract/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Thread_.NET.DAL.Context;
+
+namespace Thread_.NET.DAL.Repositories.Abstract
+{
+    public abstract class BaseRepository
+    {
+        private protected readonly ThreadContext _context;
+
+        public BaseRepository(ThreadContext context)
+        {
+            _context = context;
+        }
+    }
+}
