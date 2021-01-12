1f08476a1015db34525692a4736b8686e8a108e3
Sergey <sergey_chizhik@ukr.net>
2021-01-12 15:02:41 +0200

    Create IPostRepository.cs

---
+++ backend/Thread .NET.DAL/Repositories/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Thread_.NET.DAL.Entities;
+
+namespace Thread_.NET.DAL.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<ICollection<Post>> GetPosts(int threadSize, int threadPage, bool isOnlyMine, int userId);
+        Task<ICollection<Post>> GetAllPosts(int userId);
+        Task<Post> GetPostById(int postId);
+        Task<Post> CreatePost(Post post);
+        Task<int> UpdatePost(Post post);
+        Task<bool> DeletePost(int postId);
+    }
+}
